65ba995173a58c14bb986f55f5a934ffe7424668
Jason A. Clark <jasclark@gmail.com>
2016-03-14 23:56:15 -0600

    set register and scope of serviceworker

--- meta/scripts/global.js
+++ meta/scripts/global.js
@@ -1,7 +1,7 @@
 //register a service-worker
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/serviceworker.js', {
-        scope: '/'
+    navigator.serviceWorker.register('/~jason/serviceworker.js', {
+        scope: '/~jason/'
     });
     window.addEventListener('load', function() {
     	if (navigator.serviceWorker.controller) {
