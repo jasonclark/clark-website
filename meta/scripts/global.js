a414a47b8358792adf6d6c9fbea55c1593417297
Jason A. Clark <jasclark@gmail.com>
2016-03-14 23:46:06 -0600

    set up /scripts/ folder & global.js

---
+++ meta/scripts/global.js
@@ -0,0 +1,11 @@
+//register a service-worker
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/serviceworker.js', {
+        scope: '/'
+    });
+    window.addEventListener('load', function() {
+    	if (navigator.serviceWorker.controller) {
+    		navigator.serviceWorker.controller.postMessage({'command': 'trimCaches'});
+    	}
+    });
+}
