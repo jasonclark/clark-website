64ec01f01056f21cb963f1cbad9e6576ee606259
Jason A. Clark <jasclark@gmail.com>
2016-03-20 09:25:41 -0600

    removed trimCache function & simplified ServiceWorker registation

--- meta/scripts/global.js
+++ meta/scripts/global.js
@@ -1,13 +1,9 @@
-//register a service-worker
-if (navigator.serviceWorker) {
-	console.log('CLIENT: service worker registration in progress.');
-	navigator.serviceWorker.register('serviceworker.js', {
-        scope: '/~jason/'
-    });
-    window.addEventListener('load', function() {
-    	if (navigator.serviceWorker.controller) {
-    		navigator.serviceWorker.controller.postMessage({'command': 'trimCaches'});
-    	}
-    });
-    console.log('CLIENT: service worker registration complete.');
-}
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('serviceworker.js', { scope: '/~jason/' }).then(function(reg) {
+	//registration worked
+	console.log('Registration succeeded. Scope is ' + reg.scope);
+	}).catch(function(error) {
+		//registration failed
+		console.log('Registration failed with ' + error);
+	});
+};
