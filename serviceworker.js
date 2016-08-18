26b5803e50f8d24700ff3b4b20f79e919b7cfe49
jasonclark <jasclark@gmail.com>
2016-08-17 23:40:37 -0400

    rewrote serviceworker - simplified using discrete functions, removed offline image

--- serviceworker.js
+++ serviceworker.js
@@ -1,122 +1,87 @@
 'use strict';
 
-(function() {
+const version = 'v0.01::';
+const staticCacheName = version + 'static';
 
-    // Update 'version' if you need to refresh the cache
-    var staticCacheName = 'static';
-    var version = 'v1::';
+function updateStaticCache() {
+    return caches.open(staticCacheName)
+        .then( cache => {
+            // These items must be cached for the Service Worker to complete installation
+            return cache.addAll([
+                '/~jason/meta/scripts/global.js',
+                '/~jason/meta/styles/global.css',
+                '/~jason/meta/fonts/Open-Sans-700/Open-Sans-700.ttf',
+                '/~jason/meta/fonts/Open-Sans-700/Open-Sans-700.woff',
+                '/~jason/meta/fonts/Open-Sans-regular/Open-Sans-regular.ttf',
+                '/~jason/meta/fonts/Open-Sans-regular/Open-Sans-regular.woff',
+                '/~jason/meta/img/catalog-card.png',
+                '/~jason/meta/img/catalog-card-small.png',
+                '/~jason/meta/img/clark-profile.jpg',
+                '/~jason/meta/img/dog.jpg',
+                '/~jason/404.html',
+                '/~jason/about.html',
+                '/~jason/code.html',
+                '/~jason/contact.html',
+                '/~jason/index.html',
+                '/~jason/resume.html',
+                '/~jason/search.html',
+                '/~jason/sitemap.html',
+                '/~jason/talks.html',
+                '/~jason/files/clark-jason-cv.pdf',
+                '/',
+                '/~jason/offline.html'
+            ]);
+        });
+}
 
-    // Store core files in a cache (including a page to display when offline)
-    function updateStaticCache() {
-        return caches.open(version + staticCacheName)
-            .then(function (cache) {
-                return cache.addAll([
-                    '/~jason/meta/scripts/global.js',
-                    '/~jason/meta/styles/global.css',
-                    '/~jason/meta/fonts/Open-Sans-700/Open-Sans-700.ttf',
-                    '/~jason/meta/fonts/Open-Sans-700/Open-Sans-700.woff',
-                    '/~jason/meta/fonts/Open-Sans-regular/Open-Sans-regular.ttf',
-                    '/~jason/meta/fonts/Open-Sans-regular/Open-Sans-regular.woff',
-                    '/~jason/meta/img/catalog-card.png',
-                    '/~jason/meta/img/catalog-card-small.png',
-                    '/~jason/meta/img/clark-profile.jpg',
-                    '/~jason/meta/img/dog.jpg',
-                    '/~jason/404.html',
-                    '/~jason/about.html',
-                    '/~jason/code.html',
-                    '/~jason/contact.html',
-                    '/~jason/index.html',
-                    '/~jason/resume.html',
-                    '/~jason/search.html',
-                    '/~jason/sitemap.html',
-                    '/~jason/talks.html',
-                    '/~jason/files/clark-jason-cv.pdf',
-                    '/',
-                    '/~jason/offline.html'
-                ]);
-            });
-    };
-
-    self.addEventListener('install', function (event) {
-        event.waitUntil(updateStaticCache());
-    });
-
-    self.addEventListener('activate', function (event) {
-        event.waitUntil(
-            caches.keys()
-                .then(function (keys) {
-                    // Remove caches whose name is no longer valid
-                    return Promise.all(keys
-                        .filter(function (key) {
-                          return key.indexOf(version) !== 0;
-                        })
-                        .map(function (key) {
-                          return caches.delete(key);
-                        })
-                    );
-                })
-        );
-    });
-
-    self.addEventListener('fetch', function (event) {
-        var request = event.request;
-        // Always fetch non-GET requests from the network
-        if (request.method !== 'GET') {
-            event.respondWith(
-                fetch(request)
-                    .catch(function () {
-                        return caches.match('/~jason/offline.html');
-                    })
+// Remove caches whose name is no longer valid
+function clearOldCaches() {
+    return caches.keys()
+        .then( keys => {
+            return Promise.all(keys
+                .filter(key => key.indexOf(version) !== 0)
+                .map(key => caches.delete(key))
             );
-            return;
-        }
+        });
+}
 
-        // For HTML requests, try the network first, fall back to the cache, finally the offline page
-        if (request.headers.get('Accept').indexOf('text/html') !== -1) {
-            // Fix for Chrome bug: https://code.google.com/p/chromium/issues/detail?id=573937
-            if (request.mode != 'navigate') {
-                request = new Request(request.url, {
-                    method: 'GET',
-                    headers: request.headers,
-                    mode: request.mode,
-                    credentials: request.credentials,
-                    redirect: request.redirect
-                });
-            }
-            event.respondWith(
-                fetch(request)
-                    .then(function (response) {
-                        // Stash a copy of this page in the cache
-                        var copy = response.clone();
-                        caches.open(version + staticCacheName)
-                            .then(function (cache) {
-                                cache.put(request, copy);
-                            });
+self.addEventListener('install', event => {
+    event.waitUntil(updateStaticCache()
+        .then( () => self.skipWaiting() )
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clearOldCaches()
+        .then( () => self.clients.claim() )
+    );
+});
+
+self.addEventListener('fetch', event => {
+    let request = event.request;
+    let url = new URL(request.url);
+
+    // Ignore requests to some directories
+    if (request.url.indexOf('/~jason/dev/') !== -1 || request.url.indexOf('/~jason/files/') !== -1) {
+        return;
+    }
+
+    // Look in the cache first, fall back to the network, finally offline page
+    event.respondWith(
+        caches.match(request)
+            .then(response => {
+                // CACHE
+                return response || fetch(request)
+                    .then( response => {
+                        // NETWORK
                         return response;
                     })
-                    .catch(function () {
-                        return caches.match(request)
-                            .then(function (response) {
-                                return response || caches.match('/~jason/offline.html');
-                            })
+            })
+            .catch(function () {
+                return caches.match(request)
+                    .then(function (response) {
+                        return response || caches.match('/~jason/offline.html');
                     })
-            );
-            return;
-        }
-
-        // For non-HTML requests, look in the cache first, fall back to the network
-        event.respondWith(
-            caches.match(request)
-                .then(function (response) {
-                    return response || fetch(request)
-                        .catch(function () {
-                            // If the request is for an image, show an offline placeholder
-                            if (request.headers.get('Accept').indexOf('image') !== -1) {
-                                return new Response('<svg width="400" height="300" role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml' }});
-                            }
-                        });
-                })
-        );
-    });
-
-})();
+            })
+    );
+});
