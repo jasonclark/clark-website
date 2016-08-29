61bd5b30c1742cffd45c5102ab701a95234367c8
jasonclark <jasclark@gmail.com>
2016-08-29 15:40:24 -0400

    switched to implicit conditional for navigator.serviceWorker

--- meta/scripts/global.js
+++ meta/scripts/global.js
@@ -1,4 +1,5 @@
-if ('serviceWorker' in navigator) {
+//register the service-worker
+if (navigator.serviceWorker) {
 	navigator.serviceWorker.register('serviceworker.js', { scope: '/~jason/' }).then(function(reg) {
 	//registration worked
 	console.log('Registration succeeded. Scope is ' + reg.scope);
