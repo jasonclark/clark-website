637f7bc13779a7adfa981b495f5ab583bc15cafb
jasonclark <jasclark@gmail.com>
2017-05-15 14:09:20 -0400

    standardized whitespace and comments

--- meta/scripts/global.js
+++ meta/scripts/global.js
@@ -1,10 +1,12 @@
-//register the service-worker
+// register the service-worker
 if (navigator.serviceWorker) {
-	navigator.serviceWorker.register('serviceworker.js', { scope: '/~jason/' }).then(function(reg) {
-	//registration worked
-	console.log('Registration succeeded. Scope is ' + reg.scope);
-	}).catch(function(error) {
-		//registration failed
-		console.log('Registration failed with ' + error);
-	});
+  navigator.serviceWorker.register('serviceworker.js', {
+    scope: '/~jason/'
+  }).then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
 };
