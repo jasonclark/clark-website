3d3566d463cd141bf03e2f2424c95732675cdb56
jasonclark <jasclark@gmail.com>
2017-05-17 14:38:14 -0400

    added javascript to highlight current page in <nav>

--- meta/scripts/global.js
+++ meta/scripts/global.js
@@ -1,3 +1,15 @@
+// set active <nav> style for current page
+var navLinks = document.getElementById("nav").getElementsByTagName("a");
+console.log(navLinks);
+// setting active to 0 will highlight first nav item as current
+let active = 0;
+for (let i = 0; i < navLinks.length; i++) {
+    if (navLinks[i].href === document.URL) {
+        active = i;
+    }
+}
+navLinks[active].className = "current";
+
 // register the service-worker
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register('serviceworker.js', {
